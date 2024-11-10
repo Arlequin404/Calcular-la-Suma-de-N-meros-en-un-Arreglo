06648433e7902f8bdd82b8733fcb075419628e7f
Alex Ortis <87336367+Arlequin404@users.noreply.github.com>
2024-11-10 01:59:27 -0500

    se modifico el codigo

--- go-factorial.go
+++ go-factorial.go
@@ -1,22 +1,9 @@
+// hello_world.go
 package main
 
 import "fmt"
 
-/**
- * Función para calcular el factorial de un número.
- */
-func factorial(n int) int {
-    if n == 0 {
-        return 1
-    }
-    return n * factorial(n-1)
-}
-
 func main() {
-    var number int
-    fmt.Println("Enter a number to calculate its factorial:")
-    fmt.Scan(&number)
-    
-    result := factorial(number)
-    fmt.Printf("The factorial of %d is: %d\n", number, result)
+    greetingMessage := "Hello, World!"
+    fmt.Println(greetingMessage)
 }
